a0718fb2937b65a185b719c5ea35e910dd497097
chromecrown <chromecrown@hotmail.com>
2016-04-23 22:08:46 +0800

    #

--- src/main/java/com/xx/test/mt/common/cache/redis/IRedisCache.java
+++ src/main/java/com/xx/test/mt/common/cache/redis/IRedisCache.java
@@ -1,6 +1,7 @@
 package com.xx.test.mt.common.cache.redis;
 
-public interface RedisCache {
+public interface IRedisCache {
+	
 	public <T> T getRedisCacheInfo(String key);
 	public <T> boolean setRedisCacheInfo(String key,T value);
 }
--- src/main/java/com/xx/test/mt/common/cache/redis/RedisCacheManager.java
+++ src/main/java/com/xx/test/mt/common/cache/redis/RedisCacheManager.java
@@ -2,11 +2,11 @@ package com.xx.test.mt.common.cache.redis;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import redis.clients.jedis.ShardedJedis;
 import redis.clients.jedis.ShardedJedisPool;
 
-public class RedisCacheManager implements RedisCache{
+public class RedisCacheManager implements IRedisCache{
+	
 	protected Log log = LogFactory.getLog(RedisCacheManager.class);
 	
 	private ShardedJedisPool pool;
@@ -15,30 +15,12 @@ public class RedisCacheManager implements RedisCache{
 		this.pool = pool;
 	}
 
-	@SuppressWarnings("deprecation")
 	public <T> T getRedisCacheInfo(String key) {
-		try{
-			log.info("get from redisCache:"+key);
-			System.out.println("get from rediscache");
-			ShardedJedis jedis = pool.getResource();
-			pool.returnResource(jedis);
-			return (T)jedis.get(key);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 		return null;
 	}
 
 	public <T> boolean setRedisCacheInfo(String key, T value) {
-		try{
-			log.info("add to redisCache:"+key);
-			System.out.println("add to rediscache");
-			ShardedJedis jedis = pool.getResource();
-			jedis.set(key, (String)value);
-			pool.returnResource(jedis);
-		}catch(Exception e){
-			e.printStackTrace();
-		}
 		return false;
 	}
+
 }
