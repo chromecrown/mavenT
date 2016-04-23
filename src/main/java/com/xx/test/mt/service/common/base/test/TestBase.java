072bc6f1ff3d036a69fe290f98d39cb63a2f7ae3
chromecrown <chromecrown@hotmail.com>
2016-04-23 09:18:15 +0800

    #

---
+++ src/main/java/com/xx/test/mt/service/common/base/test/TestBase.java
@@ -0,0 +1,25 @@
+package com.xx.test.mt.service.common.base.test;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { 
+		"file:src/main/webapp/WEB-INF/config/spring/applicationContext.xml",
+		"file:src/main/webapp/WEB-INF/config/spring/springMVC-servlet.xml"
+		})
+public class TestBase {
+	protected Log logger = LogFactory.getLog(TestBase.class);
+
+	/**
+	 * some initial code will be here
+	 */
+	@Before
+	public void before() {
+
+	}
+}
