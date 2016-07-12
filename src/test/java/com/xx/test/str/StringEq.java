fdfec201bf2e9f5295f83b4d8b3e9b94d77e6b83
chromecrown <chromecrown@hotmail.com>
2016-07-12 10:41:34 +0800

    String class test

---
+++ src/test/java/com/xx/test/str/StringEq.java
@@ -0,0 +1,16 @@
+package com.xx.test.str;
+
+public class StringEq {
+
+	public static void main(String[] args) {
+		String s1 = "Programming";
+		String s2 = new String("Programming");
+		String s3 = "Program" + "ming";
+		
+		System.out.println(s1 == s2);
+		System.out.println(s1 == s3);
+		System.out.println(s1 == s1.intern());
+		
+	}
+
+}
