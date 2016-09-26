410d574805a7014292a7639baf670350452fbb41
chromecrown <chromecrown@hotmail.com>
2016-09-26 15:07:13 +0800

    #

--- src/test/java/com/xx/test/arithmetic/SortArithmetic.java
+++ src/test/java/com/xx/test/arithmetic/SortArithmetic.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import org.apache.commons.lang.ArrayUtils;
 
 /**
+ * 快速排序算法思路
  * 1、以第一个关键字K1为控制字，将[k1,k2,...,kn]分成两个子区，
  * 使左区所有关键字小于等于k1,右区所有关键字大于等于k1，
  * 最后控制字居两个子区中间的适当位置。在子区内数据尚处于无序状态。
@@ -15,16 +16,36 @@ import org.apache.commons.lang.ArrayUtils;
 public class SortArithmetic {
 
 	public static void main(String[] args) {
-		Integer[] a = new Integer[100];
+		Integer[] a = new Integer[20];
 		try {
-			a = RandomGen.getRandom(1, 200, 100);
+			a = RandomGen.getRandom(1, 200, 20);
 			System.out.println("a==>"+Arrays.toString(a));
 			int[] b = ArrayUtils.toPrimitive(a);
 			quicksort(b, 0, b.length-1);
+			//bubbleSort(b);
 			System.out.println("b==>"+Arrays.toString(b));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * 冒泡排序
+	 * 按照由小到大的升序排列
+	 * 
+	 * @param data
+	 */
+	public static int[] bubbleSort(int[] data){
+		for(int i=0; i<data.length; i++){
+			for(int j=i+1; j<data.length; j++){
+				if(data[j] < data[i]){
+					int tmp = data[j];
+					data[j] = data[i];
+					data[i] = tmp;
+				}
+			}//end in for
+		} //end out for
+		return data;
 	}
 	
 	/**
@@ -68,4 +89,6 @@ public class SortArithmetic {
 			quicksort(n, dp + 1, right);
 		}
 	}
+	
+	
 }
