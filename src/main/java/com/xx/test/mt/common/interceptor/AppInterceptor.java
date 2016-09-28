f143e8cb3ec3a9471eabd21d3005d0a18607a72f
chromecrown <chromecrown@hotmail.com>
2016-09-28 20:22:13 +0800

    拦截器修改

--- src/main/java/com/xx/test/mt/common/interceptor/AppInterceptor.java
+++ src/main/java/com/xx/test/mt/common/interceptor/AppInterceptor.java
@@ -1,7 +1,5 @@
 package com.xx.test.mt.common.interceptor;
 
-import java.net.URL;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -33,32 +31,22 @@ public class AppInterceptor implements HandlerInterceptor {
 		String contextPath = request.getContextPath();
 		String loginURL = contextPath + "/login";//login url
 		String uri = request.getRequestURI();
-		System.out.println("==++uri:" + uri + "contextPath:" + contextPath);
 		HttpSession session = request.getSession();
-		System.out.println("class AppInterceptor,method preHandle->contextPath:" + contextPath+" uri:" + uri + " isAsync:"+isAsync);
-		System.out.println("uri.indexOf(/login)------" + uri.indexOf("/login"));
-		int indexOfLogin = uri.indexOf("/login");
-		int indexOfDologin = uri.indexOf("/dologin");
-		//login action
-		if(indexOfLogin != -1 | indexOfDologin != -1){
-			System.out.println("====== is do login------");
-			isGoOn = true;
-			return isGoOn;
+		System.out.println("class AppInterceptor,method preHandle->contextPath:" + 
+		    contextPath+" uri:" + uri + " isAsync:"+isAsync);
+		Object obj = session.getAttribute("logined");//get if has logged flag
+		if(null == obj){
+			System.out.println("拦截器====== will sendRedirect to login page------");
+			response.sendRedirect(loginURL);
 		}else{
-			Object obj = session.getAttribute("logined");//get if has logged flag
-			if(null == obj){
-				System.out.println("====== will sendRedirect to login page------");
+			boolean isLogined = (boolean)obj;
+			System.out.println("obj logined:===>" + obj);
+			if(isLogined == false){
+				isGoOn = false;
 				response.sendRedirect(loginURL);
+				return isGoOn;
 			}else{
-				boolean isLogined = (boolean)obj;
-				System.out.println("obj:===>" + obj);
-				if(isLogined == false){
-					isGoOn = false;
-					response.sendRedirect(loginURL);
-					return isGoOn;
-				}else{
-					return true;
-				}
+				return true;
 			}
 		}
 		return isGoOn;
