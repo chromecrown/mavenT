196abf563f85fcddb55b07792c541662d957a430
unknown <chromecrown@hotmail.com>
2021-11-22 09:36:49 +0800

    modify the test case of call py script

--- src/test/java/com/xx/test/hutool/HuToolTest.java
+++ src/test/java/com/xx/test/hutool/HuToolTest.java
@@ -9,7 +9,9 @@ public class HuToolTest {
 	@Test
 	public void testJava8MethodRef(){
 		// 调用系统命令
-		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopSzlcsc.py");
+		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopSzlcsc.py -u " + "https://item.szlcsc.com/139811.html");
+//		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopSzlcsc.py -u "
+//						+ "https://item.szlcsc.com/1045745.html");
 		System.out.println(str);
 	}
 }
