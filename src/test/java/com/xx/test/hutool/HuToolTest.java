7252947e2fbd61060bbb49b9c8e5439526d85583
chromecrown <chromecrown@users.noreply.github.com>
2021-11-23 10:28:45 +0800

    Update HuToolTest.java
    fix: 添加测试用例，在java中调用python脚本并获取控制台打印结果

--- src/test/java/com/xx/test/hutool/HuToolTest.java
+++ src/test/java/com/xx/test/hutool/HuToolTest.java
@@ -7,15 +7,45 @@ import cn.hutool.core.util.RuntimeUtil;
 public class HuToolTest {
 	
 	/**
+	 * - 获取【立创商城】指定页面的价格信息
 	 * - 运行环境，安装python3，加入化境变量
 	 * - maven依赖hutool包
 	 */
 	@Test
-	public void testJava8MethodRef(){
+	public void testJavaCallPyGetPriceOfSzlcsc(){
 		// 调用系统命令
 		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopSzlcsc.py -u " + "https://item.szlcsc.com/139811.html");
-//		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopSzlcsc.py -u "
-//						+ "https://item.szlcsc.com/1045745.html");
-		System.out.println(str);
+		System.out.println("【立创商城】价格信息：\n" + str);
+	}
+	
+	/**
+	 * - 获取【云汉芯城】指定页面的价格信息
+	 * - 没有获取成功
+	 */
+	@Test
+	public void testJavaCallPyGetPriceOfIckey(){
+		// 调用系统命令
+		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopIckey.py -u " + "https://www.ickey.cn/detail/1003001428067342/PE2512FKF7W0R008Z.html");
+		System.out.println("云汉芯城价格信息：" + str);
+	}
+	
+	/**
+	 * - 获取【华秋商城】指定页面的价格信息
+	 */
+	@Test
+	public void testJavaCallPyGetPriceOfHqchips(){
+		// 调用系统命令
+		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopHqchips.py -u " + "https://www.hqchip.com/p/ACPL-C87AT-500E.html");
+		System.out.println("华秋商城价格信息：" + str);
+	}
+	
+	/**
+	 * - 获取【硬之城】指定页面的价格信息
+	 */
+	@Test
+	public void testJavaCallPyGetPriceOfAllChips(){
+		// 调用系统命令
+		String str = RuntimeUtil.execForStr("python F:\\Workspaces\\eclipse202109\\pyGrabShop\\com\\zq\\py\\BrowseShopAllChips.py -u " + "https://www.allchips.com/detail84237595");
+		System.out.println("【硬之城】价格信息：" + str);
 	}
 }
